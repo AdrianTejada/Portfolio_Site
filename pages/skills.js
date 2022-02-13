c73303e1a224f1f2aff8ad5dfa9f0b02b85f0010
AdrianTejada <atejada@my.bcit.ca>
2022-02-13 12:15:00 -0800

    starting skill set

--- pages/skills.js
+++ pages/skills.js
@@ -3,18 +3,21 @@ import styled from "styled-components"
 
 import { Menu } from "@/comps/Menu"
 import { Header } from "@/comps/Header"
+import {Text} from '@/comps/Text'
 
 const MainCont = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
-const Line = styled.div`
-    width: 100%;
-    height: 20px;
-    background-color: black;
+const TextCont = styled.div`
+    width: 90%;
+    margin-top: 30px;
 `
+
 
 
 export default function Skills ({
@@ -24,10 +27,18 @@ export default function Skills ({
 
     return <MainCont>
         <Menu currentRoute="skills"/>
+
             <Header
                 erase={false}
                 text="My Skill Set"
                 />
-        <Line/>
+            <TextCont>
+                <Text
+                    erase={false}
+                    text="My primary skill is Front-End Development using React, HTML, CSS, & Javascript with sufficient knowledge in UI/UX Design. I also enjoy bringing Graphic Design to life through Motion Graphic."
+                />
+            </TextCont>
+
+            
     </MainCont>
 }
